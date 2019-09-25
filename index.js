0c19d590cc20923791a8dfe6f5ea11c602b24866
Bill G <gbilly3@gmail.com>
2019-09-25 00:20:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const currentUser = 'Grace Hopper';
 
-...
+
 
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
